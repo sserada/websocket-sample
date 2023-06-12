2d106fb1efc57d3556b5c5706146ec0710d47456
fightingsou <hirawatasou@gmail.com>
2023-06-12 15:39:55 +0900

    :recycle: Refactor WebSocket endpoint for image processing
    This commit refactors the WebSocket endpoint in the backend/main.py file to enable image processing. The changes include:
    
    - Added import statements for the necessary modules:
      - pydantic
      - base64
      - cv2 (OpenCV)
      - numpy (np)
    
    - Updated the  function:
      - Added the  statement to keep track of connected clients.
      - Implemented a loop to continuously receive JSON data from the WebSocket.
      - Extracted the image chunk from the received data and performed the following operations on it:
        - Split the format and image data.
        - Decoded the base64-encoded image data.
        - Decoded the image using OpenCV (cv2) and converted it to grayscale.
        - Encoded the grayscale image as a JPEG (.jpg) buffer using OpenCV (cv2).
        - Encoded the JPEG buffer as base64 and converted it to a string.
        - Reassembled the format and image data and updated the  field.
        - Printed the modified data.
        - Sent the modified data back to the WebSocket client using .
    
    - Added exception handling to catch any errors that may occur during the image processing and prevent the WebSocket connection from closing prematurely.
    
    These changes improve the functionality of the WebSocket endpoint, allowing for image processing tasks to be performed on the received data.

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, WebSocket
 from typing import List
+from pydantic import BaseModel
 import uvicorn
+import base64
+import cv2
+import numpy as np
 
 app = FastAPI()
 connected_clients = set()
@@ -8,16 +12,24 @@ connected_clients = set()
 @app.websocket('/ws')
 async def upload_endpoint(websocket: WebSocket):
     await websocket.accept()
-    base64_data = b''
-    while True:
-        data = await websocket.receive_text()
-        # 処理
-        print(data)
-        await websocket.send_text(data)
-        break
+    connected_clients.add(websocket)
+    try:
+        while True:
+            data = await websocket.receive_json()
+            image = data['chunk']
+            format, image = image.split(',')
+            image = base64.b64decode(image)
+            image = cv2.imdecode(np.frombuffer(image, np.uint8), cv2.IMREAD_COLOR)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            _, buffer = cv2.imencode('.jpg', image)
+            image = base64.b64encode(buffer).decode('utf-8')
+            data['chunk'] = format + ',' + image
+            print(data)
+            await websocket.send_json(data)
+    except:
+        pass
 
-    base64_data += data.encode('utf-8')
-    print(base64_data)
+    connected_clients.remove(websocket)
     await websocket.close()
 
 
